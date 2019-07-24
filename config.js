016477ef82677304593f133b021c94fa08b3fce1
itanwat <itanwat@ap.xom.com>
2019-07-24 19:54:03 +0700

    initial

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     // Your queueName (min 3 char, max 65) - will be automatically created in sender.js
     queueName: 'helloq',
     // Go to storage account page --> Storage account name
-    azureStorageAccount: 'isarastorageaccount',
+    azureStorageAccount: 'xomisarastorageacc',
     // Go to storage account page --> Access Keys
-    azureStorageAccessKey: '0LktxoPMiZUMU6YKhE8ORZYk3ItyXsgb0NYDkYR9hKgPYjGVvMhgI7jknvzkGA47C5TNJxoYZL4wFIZpNARFaw==',
+    azureStorageAccessKey: 'zsJhEk8/ev4yiQDRu0oAaQU/yVnI32EWjv8ct1gno2YwOoKCI5MyZhviwH7hatrx6t3sFskms5wX5fG3uzeVcQ==',
   };
