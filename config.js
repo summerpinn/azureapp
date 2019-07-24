9cdf83c2bfc311ace5d3d6c8a2369c20664b35aa
itanwat <itanwat@ap.xom.com>
2019-07-24 11:41:53 +0700

    initial

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // Your queueName (min 3 char, max 65) - will be automatically created in sender.js
+    queueName: 'helloq',
+    // Go to storage account page --> Storage account name
+    azureStorageAccount: 'isarastorageaccount',
+    // Go to storage account page --> Access Keys
+    azureStorageAccessKey: '0LktxoPMiZUMU6YKhE8ORZYk3ItyXsgb0NYDkYR9hKgPYjGVvMhgI7jknvzkGA47C5TNJxoYZL4wFIZpNARFaw==',
+  };
---
+++ index.js
@@ -0,0 +1,34 @@
+var azure = require('azure-storage');
+const express = require('express');
+var config = require('./config');
+const queueService = azure.createQueueService(config.azureStorageAccount, config.azureStorageAccessKey);
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    console.log(`[Queue - Receiver] Truncating Queue`);
+    var retMsg = ""; 
+    queueService.getMessage(config.queueName, (err, results, res) => {
+        if (err) {
+            console.log(err);
+            retMsg = err; 
+            return;
+        }
+
+        if (!results[0]) {
+            console.log("Queue is empty...");
+            retMsg = "Queue is empty..."; 
+            return;
+        }
+
+        results.foreach((result) => {
+            console.log("msg : " + result + "\n");
+            retMsg += "{" + result + "}"; 
+        });
+    });
+    res.send("msg :" + retMsg ); 
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
